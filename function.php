7420c7fce63d94554da10774e0ad6092693ddc30
YuanIsyaRahmawati <68942297+YuanIsyaRahmawati@users.noreply.github.com>
2020-08-24 08:43:59 -0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,17 @@
+<?php 
+function registrasi($data) {
+	global $conn
+
+	$username = strtolower (stripcslashes($data["username"]));
+	$password = mysqli_real_escape_string($conn, $data["password"]);
+	$password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+	// cek konfirmasi password
+	if ( &password !== $password2) {
+		echo "<script>
+				alert('konfirmasi password tidak sesuai');
+			 </script>";
+	    return false;
+	}	
+}
+ ?>
---
+++ registrasi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Registrasi</title>
+</head>																														
+<body>
+
+<h1> Halaman Registrasi</h1>
+<form action="" method="post">
+
+	<ul>
+		<li>
+			<label for="username">username :</label>
+			<input type="text" name="username" id="username">
+		</li>
+		<li>
+			<label for="password">password :</label>
+			<input type="password" name="password" id="password">
+		</li>
+		<li>
+			<label for="password2">konfirmasi password :</label>
+			<input type="password" name="password" id="password">
+		</li>
+		<li>
+			<button type="submit" name="register">Register!</button>
+		</li>
+	</ul>
+	
+</form>
+
+</body>
+</html>
